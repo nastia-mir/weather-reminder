f438e9850b2d5517318708c7229196ae0fec8c94
Anastasiia <nastasia.ua@gmail.com>
2023-01-12 16:47:05 +0200

    task 17

--- backend/weatherreminder/weatherapp/serializers.py
+++ backend/weatherreminder/weatherapp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import MyUser, City
+from .models import MyUser
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -7,4 +7,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = MyUser
-        fields = ['id', 'email', 'is_staff', 'cities']
+        fields = ['id', 'email', 'cities']
--- backend/weatherreminder/weatherapp/views.py
+++ backend/weatherreminder/weatherapp/views.py
@@ -9,7 +9,7 @@ import requests
 from datetime import date
 
 from .models import MyUser, City
-from .serializers import UserSerializer, CitySerializer
+from .serializers import UserSerializer
 API_KEY = '31ee71338fd9a262442351ab26c5707e'
 
 
@@ -31,41 +31,27 @@ class HomeView(APIView):
 
     def get(self, request):
         url = 'http://api.openweathermap.org/data/2.5/weather'
-        params = {'q': 'kyiv', 'appid': API_KEY, 'units': 'metric'}
+        user = MyUser.objects.get(id=request.user.id)
+        cities = UserSerializer(user).data['cities']
         context = {}
 
-        r = requests.get(url=url, params=params)
-        result = r.json()
+        for city in cities:
+            params = {'q': city, 'appid': API_KEY, 'units': 'metric'}
+            r = requests.get(url=url, params=params)
+            result = r.json()
+            city_context = {}
 
-        if result['cod'] == 200:
-            context['city'] = result['name']
-            context['weather'] = result['weather'][0]['main']
-            context['description'] = result['weather'][0]['description']
-            # context['icon'] = result['weather'][0]['icon']
-            context['temp'] = result['main']['temp']
-            context['date'] = date.today()
-        elif result['cod'] == 404:
-            context['city'] = None
+            if result['cod'] == 200:
+                city_context['city'] = result['name']
+                city_context['weather'] = result['weather'][0]['main']
+                city_context['description'] = result['weather'][0]['description']
+                # city_context['icon'] = result['weather'][0]['icon']
+                city_context['temp'] = result['main']['temp']
+                city_context['date'] = date.today()
+            elif result['cod'] == '404':
+                city_context['city'] = None
 
-        return Response(context)
-
-    def post(self, request):
-        url = 'http://api.openweathermap.org/data/2.5/weather'
-        params = {'appid': API_KEY, 'units': 'metric', 'q': request.data["city"]}
-
-        r = requests.get(url=url, params=params)
-        result = r.json()
-        context = {}
-
-        if result['cod'] == 200:
-            context['city'] = result['name']
-            context['weather'] = result['weather'][0]['main']
-            context['description'] = result['weather'][0]['description']
-            # context['icon'] = result['weather'][0]['icon']
-            context['temp'] = result['main']['temp']
-            context['date'] = date.today()
-        elif result['cod'] == 404:
-            context['city'] = None
+            context[city] = city_context
 
         return Response(context)
 
@@ -79,11 +65,32 @@ class SubscriptionsView(APIView):
         return Response(serialized.data)
 
     def post(self, request):
+        url = 'http://api.openweathermap.org/data/2.5/weather'
         city = request.data['city']
-        subsctiption = City(user=request.user, city=city)
-        subsctiption.save()
-        users = MyUser.objects.get(id=request.user.id)
-        serialized = UserSerializer(users)
-        return Response(serialized.data)
 
+        params = {'q': city, 'appid': API_KEY, 'units': 'metric'}
+        r = requests.get(url=url, params=params)
+        result = r.json()
 
+        if result['cod'] == '404':
+            return Response({"message": "enter valid city name"})
+        else:
+            subscription = City(user=request.user, city=city)
+            subscription.save()
+            user = MyUser.objects.get(id=request.user.id)
+            serialized = UserSerializer(user)
+            return Response(serialized.data)
+
+    def delete(self, request):
+        city = request.data['city']
+        user = MyUser.objects.get(id=request.user.id)
+        if city in UserSerializer(user).data['cities']:
+
+            subscription = City.objects.get(user=request.user, city=city)
+            subscription.delete()
+            user = MyUser.objects.get(id=request.user.id)
+            serialized = UserSerializer(user)
+            return Response(serialized.data)
+
+        else:
+            return Response({"message": "subscription not found"})
