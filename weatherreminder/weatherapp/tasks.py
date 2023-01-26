acd42bedec6c82631d053db0228bf65a07ac6e2a
Anastasiia <nastasia.ua@gmail.com>
2023-01-26 17:48:54 +0200

    task 18

--- weatherreminder/weatherapp/tasks.py
+++ weatherreminder/weatherapp/tasks.py
@@ -7,15 +7,18 @@ from django.conf import settings
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
-from weatherapp.celery import app
-
 logger = get_task_logger(__name__)
 
 
-@app.task(name='send_email_subscribed_task')
-def send_email_subscribed_task(email, data):
-    logger.info("Sent email")
-
+def send_email(email, data):
     subject = 'Successfully subscribed'
     message = render_to_string("email_template.txt", data)
     email = EmailMessage(subject, message, settings.EMAIL_HOST_USER, [email])
+
+
+@shared_task()
+def send_email_subscribed_task(email, data):
+    logger.info("Sent email")
+    return send_email()
+
+
