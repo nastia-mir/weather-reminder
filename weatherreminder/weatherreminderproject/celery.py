f2fda61db556fb03ac7a5803dd7de927b4ff3903
Anastasia Mir <nastasia.ua@gmail.com>
2023-02-03 15:28:25 +0200

    defaultdict in tasks.send_scheduled_email

--- weatherreminder/weatherreminderproject/celery.py
+++ weatherreminder/weatherreminderproject/celery.py
@@ -10,26 +10,26 @@ app = Celery("weatherreminderproject")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.conf.beat_schedule = {
-    # 'send-scheduled-1': {
-    #     'task': 'send_scheduled_email',
-    #     'schedule': crontab(hour='*/1'),
-    #     'args': [1],
-    # },
-    # 'send-scheduled-3': {
-    #     'task': 'send_scheduled_email',
-    #     'schedule': crontab(hour='*/3'),
-    #     'args': [3],
-    # },
-    # 'send-scheduled-6': {
-    #     'task': 'send_scheduled_email',
-    #     'schedule': crontab(hour='*/6'),
-    #     'args': [6],
-    # },
-    # 'send-scheduled-12': {
-    #     'task': 'send_scheduled_email',
-    #     'schedule': crontab(hour='*/12'),
-    #     'args': [12],
-    # },
+    'send-scheduled-1': {
+        'task': 'send_scheduled_email',
+        'schedule': crontab(hour='*/1'),
+        'args': [1],
+    },
+    'send-scheduled-3': {
+        'task': 'send_scheduled_email',
+        'schedule': crontab(hour='*/3'),
+        'args': [3],
+    },
+    'send-scheduled-6': {
+        'task': 'send_scheduled_email',
+        'schedule': crontab(hour='*/6'),
+        'args': [6],
+    },
+    'send-scheduled-12': {
+        'task': 'send_scheduled_email',
+        'schedule': crontab(hour='*/12'),
+        'args': [12],
+    },
 
 
     'update-weather': {
